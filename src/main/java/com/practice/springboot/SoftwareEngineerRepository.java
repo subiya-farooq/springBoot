f0cc2d822a020610f21e14ae1a5087258b163d2b
subiya-Farooq <subiya06farooq@gmail.com>
2026-07-14 22:05:24 +0530

    Software Engineer Record

---
+++ src/main/java/com/practice/springboot/SoftwareEngineerRepository.java
@@ -0,0 +1,8 @@
+package com.practice.springboot;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+
+
+}
---
+++ src/main/java/com/practice/springboot/SoftwareEngineerService.java
@@ -0,0 +1,30 @@
+package com.practice.springboot;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SoftwareEngineerService {
+
+    private final SoftwareEngineerRepository softwareEngineerRepository;
+
+    public SoftwareEngineerService(SoftwareEngineerRepository softwareEngineerRepository) {
+        this.softwareEngineerRepository = softwareEngineerRepository;
+    }
+
+    public List<SoftwareEngineer> getAllSoftwareEngineer(){
+        return softwareEngineerRepository.findAll();
+    }
+
+    public void insertSoftwareEngineer(SoftwareEngineer softwareEngineer) {
+        softwareEngineerRepository.save(softwareEngineer);
+
+    }
+
+
+    public SoftwareEngineer getSoftwareEngineerById(Integer id) {
+        return softwareEngineerRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(id + " not found"));
+    }
+}
